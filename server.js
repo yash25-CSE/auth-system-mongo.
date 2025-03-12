fefbff8c5cc39cd2747bcbbe343a8dc6bbe6d213
Yash Sahejwani <sahejwaniyash3@gmail.com>
2025-03-13 00:29:38 +0530

    Updated MongoDB connection string

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
@@ -136,4 +136,5 @@ app.get("/api/user-info", async (req, res) => {
 });
 
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
